5ab2e3f2b579104029257af63f55caf29c384b3e
John Petro <john@johnpetro.com>
2022-12-01 22:55:20 -0500

    code for part 1

--- 01/pt1.pl
+++ 01/pt1.pl
@@ -1,16 +1,34 @@
 #!/usr/bin/perl
 
-
 my $filename = $ARGV[0];
 my @data = ();
 my $data = 0;
-
+my $value = 0;
 print "filename is $filename\n";
 
 open(IN,"$filename") || die "$filename can't be opened";
 
 while (<IN>){
-       chomp;
-       push (@data,$_);
+      if ( $_ != " ")
+      {
+       $value = $value + $_;
+      }
+      else{
+       push (@data,$value);
+       $value = 0;
+
+      }
 }
+push (@data,$value);
 $data = @data;
+# figure out which elf has the most calories
+
+my $highest=0;
+for (my $i=0; $i < $data ; $i++){
+       if ( $data[$i] > $highest){
+              $highest = $data[$i];
+              $elf = $i;
+       }
+}
+print "elf = $elf\n";
+print "calories = $data[$elf]\n";
